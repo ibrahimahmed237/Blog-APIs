50266be1fde54a518d2365bbd0dd2359606bd5fd
Ibrahim Ahmed <ibrahim3hmedib@gmail.com>
2023-12-15 16:01:14 +0200

    Add authentication routes and controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
-// const isAuth = require("../middlewares/is-auth");
+const isAuth = require("../middlewares/is-auth");
 
 // POST /auth/signup
 router.post("/signup", authController.signup);
@@ -9,7 +9,12 @@ router.post("/signup", authController.signup);
 // POST /auth/login
 router.post("/login", authController.login);
 
+router.post("/logout", isAuth, authController.logout);
 
+//GET /auth/status
+router.get("/status", isAuth, authController.getStatus);
 
+//PUT /auth/status
+router.put("/status", isAuth, authController.updateStatus);
 
 module.exports = router;
